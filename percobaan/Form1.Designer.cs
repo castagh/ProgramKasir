bb41c2f24292af12a2eca8b07d9b1a13d1075d5a
castagh <serkahniskala@gmail.com>
2023-03-10 09:00:32 +0700

    Add project files.

---
+++ percobaan/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace percobaan
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_stok = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_harga = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_penerbit = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_pengarang = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_judul = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.searchData = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 732);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(758, 250);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.tb_stok);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.tb_harga);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tb_penerbit);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.tb_pengarang);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.tb_judul);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.tb_id);
+            this.panel1.Location = new System.Drawing.Point(34, 114);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(758, 455);
+            this.panel1.TabIndex = 42;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 378);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 25);
+            this.label6.TabIndex = 55;
+            this.label6.Text = "Stok";
+            // 
+            // tb_stok
+            // 
+            this.tb_stok.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_stok.Location = new System.Drawing.Point(164, 364);
+            this.tb_stok.Name = "tb_stok";
+            this.tb_stok.Size = new System.Drawing.Size(552, 39);
+            this.tb_stok.TabIndex = 54;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 25);
+            this.label5.TabIndex = 51;
+            this.label5.Text = "Harga";
+            // 
+            // tb_harga
+            // 
+            this.tb_harga.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_harga.Location = new System.Drawing.Point(164, 299);
+            this.tb_harga.Name = "tb_harga";
+            this.tb_harga.Size = new System.Drawing.Size(552, 39);
+            this.tb_harga.TabIndex = 50;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 25);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "Penerbit";
+            // 
+            // tb_penerbit
+            // 
+            this.tb_penerbit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_penerbit.Location = new System.Drawing.Point(166, 232);
+            this.tb_penerbit.Name = "tb_penerbit";
+            this.tb_penerbit.Size = new System.Drawing.Size(551, 39);
+            this.tb_penerbit.TabIndex = 48;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 25);
+            this.label4.TabIndex = 47;
+            this.label4.Text = "Pengarang";
+            // 
+            // tb_pengarang
+            // 
+            this.tb_pengarang.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_pengarang.Location = new System.Drawing.Point(165, 165);
+            this.tb_pengarang.Name = "tb_pengarang";
+            this.tb_pengarang.Size = new System.Drawing.Size(551, 39);
+            this.tb_pengarang.TabIndex = 46;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 25);
+            this.label2.TabIndex = 45;
+            this.label2.Text = "Judul";
+            // 
+            // tb_judul
+            // 
+            this.tb_judul.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_judul.Location = new System.Drawing.Point(166, 104);
+            this.tb_judul.Name = "tb_judul";
+            this.tb_judul.Size = new System.Drawing.Size(550, 39);
+            this.tb_judul.TabIndex = 44;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 25);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "Id";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_id.Location = new System.Drawing.Point(166, 42);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(550, 39);
+            this.tb_id.TabIndex = 42;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn_Add.Location = new System.Drawing.Point(75, 592);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(148, 34);
+            this.btn_Add.TabIndex = 52;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = false;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn_save.Location = new System.Drawing.Point(249, 592);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(148, 34);
+            this.btn_save.TabIndex = 53;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = false;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btn_delete.Location = new System.Drawing.Point(602, 592);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(148, 34);
+            this.btn_delete.TabIndex = 55;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = false;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(428, 592);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 34);
+            this.button2.TabIndex = 54;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 680);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 25);
+            this.label7.TabIndex = 58;
+            this.label7.Text = "Search";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(354, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 25);
+            this.label8.TabIndex = 56;
+            this.label8.Text = "Zea Mart";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // searchData
+            // 
+            this.searchData.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.searchData.Location = new System.Drawing.Point(109, 671);
+            this.searchData.Name = "searchData";
+            this.searchData.Size = new System.Drawing.Size(683, 39);
+            this.searchData.TabIndex = 59;
+            this.searchData.TextChanged += new System.EventHandler(this.searchData_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 1004);
+            this.Controls.Add(this.searchData);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Panel panel1;
+        private Label label6;
+        private TextBox tb_stok;
+        private Button btn_Add;
+        private Label label5;
+        private TextBox tb_harga;
+        private Label label3;
+        private TextBox tb_penerbit;
+        private Label label4;
+        private TextBox tb_pengarang;
+        private Label label2;
+        private TextBox tb_judul;
+        private Label label1;
+        private TextBox tb_id;
+        private Button btn_save;
+        private Button btn_delete;
+        private Button button2;
+        private Label label7;
+        private Label label8;
+        private TextBox searchData;
+    }
+}
---
+++ percobaan/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace percobaan
+{
+    public partial class Form2 : Form
+    {
+        private MySqlCommand cmd;
+        private DataSet ds;
+        private MySqlDataAdapter da;
+        private MySqlDataReader rd;
+
+        connections koneksi = new connections();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection koneksi = connections.getConnection();
+            koneksi.Open();
+
+            cmd = new MySqlCommand("Select * from login where username='" + txtUsername.Text + "' and password='" + txtPassword.Text + "'", koneksi);
+            rd = cmd.ExecuteReader();
+            rd.Read();
+            if (rd.HasRows)
+            {
+                new Form1().Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Username or Password invalid!");
+            }
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
